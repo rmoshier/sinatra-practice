c557149d317c2fe4671403734d6eefe3350aa0d7
Rachel <rmoshier@gmail.com>
2014-09-30 15:55:15 -0700

    links to postso on index page.

--- lib/post.rb
+++ lib/post.rb
@@ -11,13 +11,19 @@ class Post
 
    def initialize(array)
       @title = array[-1].split(".")[0] #this is basically your title
-      @url   = "/posts/#{@title}"
+      @date = Date.parse(array[-2])
+      @post_date = array[-2]
+      @url   = "/posts/#{@post_date}/#{@title}"
    end
 
    def self.all #we want a class method that creates instances
-     Dir.glob("views/posts/*").collect do |filepath|
+     Dir.glob("views/posts/*/*").collect do |filepath|
        a = filepath.split("/")
        new(a) #same as Post.new and self.new
      end
+   end
+
+   def self.most_recent(n)
+     all.pop(n)
    end
 end
