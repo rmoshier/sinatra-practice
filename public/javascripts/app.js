9cf357579f69fd22dd1ab76b563ebf2086f666ed
Rachel <rmoshier@gmail.com>
2014-10-01 17:12:33 -0700

    dropdown style and pudding js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,13 @@
-// This isn't real javascript - just a blank file!
+$(document).ready(function () {
+
+    $("p1").hide();
+
+    // $("#title").click(function () {
+    //   $(this).toggle();
+    // });
+
+    $( "button" ).click(function() {
+      $("p1").toggle( "slow" );
+    });
+
+});
